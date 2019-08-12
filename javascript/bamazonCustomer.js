3b28fe169176a0cd9fe94b6b133fc28c91753cc4
Natalia Malcova <nataliamalkova@yahoo.com>
2019-08-12 15:36:35 -0400

    created connection to database and products function

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "1Mana2valia!",
     database: "bamazon"
 });
 
@@ -17,9 +17,34 @@ connection.connect(function(err) {
 
 function queryProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + "|" + res[i].product_name + "|" + res[i].department_name + "|" + res[i].price + "|" + res[i].stock_quantity);
-        }
+        if(err) throw err;
+
+        console.log(res);
+
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                choices: function() {
+                    var choiceItem = [];
+                    for(var i = 0; i < res[i].length; i++) {
+                        choiceItem.push(res[i].item_id);
+                    }
+                    return choiceItem;
+                },
+            
+                message: "What is the product ID would you like to buy?"
+            },
+            {
+                name: "unit",
+                type: "input",
+                message: "How many units would you like to buy?"
+            }
+        ])
+        .then(function(answer) {
+            
+        })
     });
 }
+
 
